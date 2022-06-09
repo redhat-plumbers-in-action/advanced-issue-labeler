ba26a244ef2bb039e3de907d18bd73ce6d1fa5fd
Jan Macku <jamacku@redhat.com>
2022-06-09 12:14:18 +0200

    Use probot to write action

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import { Context, Probot } from 'probot';
+import { getInput, debug } from '@actions/core';
+
+const app = (probot: Probot) => {
+  probot.on('issues.opened', async (context: Context<'issues.opened'>) => {
+    const issueForm = JSON.parse(getInput('issue-form'));
+    const section = getInput('section');
+    debug(`Issue form: ${issueForm}`);
+
+    const response = await context.octokit.rest.issues.setLabels(
+      context.issue({ labels: issueForm[section].split(', ', 10) })
+    );
+
+    debug(`GitHub API response: ${response}`);
+  });
+};
+
+export default app;
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,10 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import { setFailed } from '@actions/core';
+import run from '@probot/adapter-github-actions';
 
-async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+import app from './app';
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
-  }
+try {
+  await run.run(app);
+} catch (error) {
+  if (error instanceof Error) setFailed(error.message);
 }
-
-run()
