14322119a792ab0e80752930ceb3d92dca65a7cb
Jan Macku <jamacku@redhat.com>
2022-06-10 09:33:17 +0200

    Add guards to avoid errors
    - Avoid error when blank issue is submited
    - Avoid error when section is empty

--- dist/app.js
+++ dist/app.js
@@ -3,7 +3,15 @@ const app = (probot) => {
     probot.on('issues.opened', async (context) => {
         const issueForm = JSON.parse(getInput('issue-form'));
         const section = getInput('section');
-        debug(`Issue form: ${issueForm}`);
+        if (!issueForm.hasOwnProperty(section)) {
+            debug(`Issue form doesn't contain section: ${section}`);
+            return;
+        }
+        const labels = issueForm[section].split(', ', 10);
+        if (labels.length === 0) {
+            debug(`Section field is empty.`);
+            return;
+        }
         const response = await context.octokit.rest.issues.addLabels(context.issue({ labels: issueForm[section].split(', ', 10) }));
         debug(`GitHub API response: ${response}`);
     });
--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,22 @@ import { getInput, debug } from '@actions/core';
 
 const app = (probot: Probot) => {
   probot.on('issues.opened', async (context: Context<'issues.opened'>) => {
-    const issueForm = JSON.parse(getInput('issue-form'));
+    const issueForm: { [key: string]: string } = JSON.parse(
+      getInput('issue-form')
+    );
     const section = getInput('section');
-    debug(`Issue form: ${issueForm}`);
+
+    if (!issueForm.hasOwnProperty(section)) {
+      debug(`Issue form doesn't contain section: ${section}`);
+      return;
+    }
+
+    const labels: string[] = issueForm[section].split(', ', 10);
+
+    if (labels.length === 0) {
+      debug(`Section field is empty.`);
+      return;
+    }
 
     const response = await context.octokit.rest.issues.addLabels(
       context.issue({ labels: issueForm[section].split(', ', 10) })
