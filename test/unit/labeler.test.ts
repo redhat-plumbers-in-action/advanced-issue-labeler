a3600811ee1ab1257b05fadba35bf573b4c46f51
Jan Macku <jamacku@redhat.com>
2022-12-12 21:39:38 +0100

    test: mark missing test cases

--- test/unit/labeler.test.ts
+++ test/unit/labeler.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, beforeEach } from 'vitest';
+import { describe, it, expect, beforeEach, test } from 'vitest';
 
 import { IssueForm } from '../../src/issue-form';
 import { Labeler } from '../../src/labeler';
@@ -13,4 +13,28 @@ describe('IssueForm Object', () => {
   it('can be instantiated', () => {
     labelers.map(item => expect(item).toBeDefined());
   });
+
+  test.todo('get config()');
+  test.todo('set config()');
+  test.todo('get inputs()');
+  test.todo('set inputs()');
+
+  test.todo('setInputs()');
+
+  test.todo('get isConfig()');
+  test.todo('set isConfig()');
+
+  test.todo('get isInputs()');
+  test.todo('set isInputs()');
+
+  test.todo('get issueForm()');
+  test.todo('set issueForm()');
+
+  test.todo('gatherLabels()');
+
+  test.todo('inputBasedLabels()');
+  test.todo('configBasedLabels()');
+
+  it.todo('is valid');
+  it.todo('is invalid');
 });
