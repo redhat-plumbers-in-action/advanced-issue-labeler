bad52153b46f5782f0a85f8a2274074ddcb6394f
Jan Macku <jamacku@redhat.com>
2022-12-12 21:47:22 +0100

    test: Extend tests for `labeler.ts`

--- test/unit/labeler.test.ts
+++ test/unit/labeler.test.ts
@@ -1,40 +1,131 @@
 import { describe, it, expect, beforeEach, test } from 'vitest';
-
 import { IssueForm } from '../../src/issue-form';
 import { Labeler } from '../../src/labeler';
 
+import {
+  ILabelerTestContext,
+  labelerContextFixture,
+} from './fixtures/labeler.fixture';
+
 describe('IssueForm Object', () => {
-  let labelers: Labeler[];
-
-  beforeEach(() => {
-    labelers = [new Labeler(new IssueForm({}))];
+  beforeEach<ILabelerTestContext>(context => {
+    context.labelers = labelerContextFixture.labelers;
+    context.invalid = labelerContextFixture.invalid;
   });
 
-  it('can be instantiated', () => {
-    labelers.map(item => expect(item).toBeDefined());
-  });
+  it<ILabelerTestContext>('can be instantiated', context =>
+    context.labelers.map(labelerItem => expect(labelerItem).toBeDefined()));
 
-  test.todo('get config()');
+  test<ILabelerTestContext>('get config()', context =>
+    context.labelers.map(labelerItem =>
+      expect(labelerItem.config).toMatchSnapshot()
+    ));
+
   test.todo('set config()');
-  test.todo('get inputs()');
+
+  test<ILabelerTestContext>('get inputs()', context =>
+    context.labelers.map(labelerItem =>
+      expect(labelerItem.inputs).toMatchSnapshot()
+    ));
+
   test.todo('set inputs()');
 
-  test.todo('setInputs()');
+  test('setInputs()', () => {
+    let labeler = new Labeler(new IssueForm({}));
+    labeler.setInputs({ section: 'section' });
+    expect(labeler.inputs).toMatchInlineSnapshot(`
+      Inputs {
+        "_blockList": undefined,
+        "_section": "section",
+        "_template": undefined,
+      }
+    `);
 
-  test.todo('get isConfig()');
+    labeler.setInputs({ blockList: [''], template: 'template' });
+    expect(labeler.inputs).toMatchInlineSnapshot(`
+      Inputs {
+        "_blockList": [
+          "",
+        ],
+        "_section": "section",
+        "_template": "template",
+      }
+    `);
+
+    labeler.setInputs({ blockList: ['block1', 'block2'] });
+    expect(labeler.inputs).toMatchInlineSnapshot(`
+      Inputs {
+        "_blockList": [
+          "block1",
+          "block2",
+        ],
+        "_section": "section",
+        "_template": "template",
+      }
+    `);
+
+    // ? Empty values doesn't overwrite exiting values
+    labeler.setInputs({ section: '', template: '', blockList: [''] });
+    expect(labeler.inputs).toMatchInlineSnapshot(`
+      Inputs {
+        "_blockList": [
+          "",
+        ],
+        "_section": "section",
+        "_template": "template",
+      }
+    `);
+
+    // ? Undefined values doesn't overwrite exiting values
+    labeler.setInputs({
+      section: undefined,
+      template: undefined,
+      blockList: undefined,
+    });
+    expect(labeler.inputs).toMatchInlineSnapshot(`
+      Inputs {
+        "_blockList": [
+          "",
+        ],
+        "_section": "section",
+        "_template": "template",
+      }
+    `);
+  });
+
+  test<ILabelerTestContext>('get isConfig()', context =>
+    context.labelers.map(labelerItem =>
+      expect(labelerItem.isConfig).toMatchSnapshot()
+    ));
+
   test.todo('set isConfig()');
 
-  test.todo('get isInputs()');
+  test<ILabelerTestContext>('get isInputs()', context =>
+    context.labelers.map(labelerItem =>
+      expect(labelerItem.isInputs).toMatchSnapshot()
+    ));
+
   test.todo('set isInputs()');
 
+  // private
   test.todo('get issueForm()');
   test.todo('set issueForm()');
 
   test.todo('gatherLabels()');
 
+  // private
   test.todo('inputBasedLabels()');
   test.todo('configBasedLabels()');
 
-  it.todo('is valid');
-  it.todo('is invalid');
+  it<ILabelerTestContext>('is valid', async context =>
+    context.labelers.map(async labelerItem => {
+      //! FIXME: There is probably a better way how to do this ...
+      labelerItem.setInputs({ section: 'section' });
+      expect(Labeler.validate(labelerItem)).resolves.toMatchObject([]);
+    }));
+
+  it<ILabelerTestContext>('is invalid', async context =>
+    context.invalid.map(async invalidItem =>
+      expect(Labeler.validate(invalidItem)).resolves.toMatchSnapshot()
+    ));
 });
