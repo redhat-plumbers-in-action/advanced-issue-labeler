6ee6fddfd744ee26b977e6a0436916f256896971
Jan Macku <jamacku@redhat.com>
2022-12-13 16:01:27 +0100

    test: extend tests for `issue-form.ts` and `labeler.ts`

--- test/unit/issue-form.test.ts
+++ test/unit/issue-form.test.ts
@@ -2,22 +2,80 @@ import { describe, it, expect, beforeEach, test } from 'vitest';
 
 import { IssueForm } from '../../src/issue-form';
 
+import {
+  IIssueFormTestContext,
+  issueFormContextFixture,
+} from './fixtures/issue-form.fixture';
+
 describe('IssueForm Object', () => {
-  let issueForms: IssueForm[];
-
-  beforeEach(() => {
-    issueForms = [new IssueForm({})];
+  beforeEach<IIssueFormTestContext>(context => {
+    context.issueForms = issueFormContextFixture.issueForms;
+    context.blockLists = issueFormContextFixture.blockLists;
+    context.invalid = issueFormContextFixture.invalid;
   });
 
-  it('can be instantiated', () => {
-    issueForms.map(item => expect(item).toBeDefined());
-  });
+  it<IIssueFormTestContext>('can be instantiated', context =>
+    context.issueForms.map(issueFormItem =>
+      expect(issueFormItem).toBeDefined()
+    ));
 
+  // private
   test.todo('get parsed()');
 
-  test.todo('isProperty()');
-  test.todo('getProperty()');
-  test.todo('getSafeProperty()');
-  test.todo('listKeywords()');
-  test.todo('isCompliant()');
+  test<IIssueFormTestContext>('isProperty()', context => {
+    context.issueForms.map(issueFormItem => {
+      expect(issueFormItem.isProperty('')).toEqual(false);
+      expect(issueFormItem.isProperty('description')).toMatchSnapshot();
+    });
+
+    let issueForm = new IssueForm({ section: 'content' });
+    expect(issueForm.isProperty('section')).toEqual(true);
+  });
+
+  test<IIssueFormTestContext>('getProperty()', context => {
+    context.issueForms.map(issueFormItem => {
+      expect(issueFormItem.getProperty('')).toBeUndefined();
+      expect(issueFormItem.getProperty('description')).toMatchSnapshot();
+    });
+
+    let issueForm = new IssueForm({ section: 'content' });
+    expect(issueForm.getProperty('section')).toEqual('content');
+  });
+
+  test<IIssueFormTestContext>('getSafeProperty()', context => {
+    context.issueForms.map(issueFormItem => {
+      expect(issueFormItem.getSafeProperty('')).toBeUndefined();
+      expect(issueFormItem.getSafeProperty('description')).toMatchSnapshot();
+    });
+
+    let issueForm = new IssueForm({ section: 'content' });
+    expect(issueForm.getSafeProperty('section')).toEqual('content');
+  });
+
+  test<IIssueFormTestContext>('listKeywords()', context => {
+    context.issueForms.map(issueFormItem =>
+      context.blockLists.map(blockListItem => {
+        expect(
+          issueFormItem.listKeywords('type', blockListItem)
+        ).toMatchSnapshot();
+        expect(
+          issueFormItem.listKeywords('component', blockListItem)
+        ).toMatchSnapshot();
+      })
+    );
+  });
+
+  test('isCompliant()', () => {
+    const issueForm = new IssueForm({});
+    // @ts-expect-error: Let's ignore a type error, it's required for testing
+    expect(issueForm.isCompliant(['item1'], undefined)).toEqual(false);
+
+    expect(issueForm.isCompliant(['item1', 'item2', 'item3'], 'item1')).toEqual(
+      true
+    );
+
+    expect(issueForm.isCompliant(['item1', 'item2', 'item3'], 'item4')).toEqual(
+      false
+    );
+  });
 });
