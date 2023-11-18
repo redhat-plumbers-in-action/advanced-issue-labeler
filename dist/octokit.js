980be657db4cc547c101b9ebd195b42ce455c5b2
Jan Macku <jamacku@redhat.com>
2023-11-18 13:02:26 +0100

    feat: add support for GitHub Enterprise

--- dist/octokit.js
+++ dist/octokit.js
@@ -2,8 +2,10 @@ import { Octokit } from '@octokit/core';
 import { config } from '@probot/octokit-plugin-config';
 const CustomOctokit = Octokit.plugin(config);
 export function getOctokit(token) {
+    var _a;
     return new CustomOctokit({
         auth: token,
+        baseUrl: (_a = process.env.GITHUB_API_URL) !== null && _a !== void 0 ? _a : 'https://api.github.com',
     });
 }
 //# sourceMappingURL=octokit.js.map
