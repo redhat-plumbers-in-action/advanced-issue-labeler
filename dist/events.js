fd1e8262fb4378c61267b339893a4faeb7056f3e
Jan Macku <jamacku@redhat.com>
2023-11-20 08:42:57 +0100

    build: update `dist/` files and `yarn.lock`

--- dist/events.js
+++
@@ -1,5 +0,0 @@
-//? DOC: https://docs.github.com/en/developers/webhooks-and-events/webhooks/webhook-events-and-payloads
-export const events = {
-    issue: ['issues.opened'],
-};
-//# sourceMappingURL=events.js.map
