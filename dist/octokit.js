9e1667e35b1418219994beaa071a9131e02ea219
Jan Macku <jamacku@redhat.com>
2023-11-24 16:12:54 +0100

    use node16 for v2

--- dist/octokit.js
+++ dist/octokit.js
@@ -1,11 +1,15 @@
 import { Octokit } from '@octokit/core';
 import { config } from '@probot/octokit-plugin-config';
+import fetch from 'node-fetch';
 const CustomOctokit = Octokit.plugin(config);
 export function getOctokit(token) {
     var _a;
     return new CustomOctokit({
         auth: token,
         baseUrl: (_a = process.env.GITHUB_API_URL) !== null && _a !== void 0 ? _a : 'https://api.github.com',
+        request: {
+            fetch,
+        },
     });
 }
 //# sourceMappingURL=octokit.js.map
