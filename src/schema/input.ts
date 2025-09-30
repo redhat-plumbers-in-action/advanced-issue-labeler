5785aeba77e4e1a0022e1999b204c37c6f7002a6
Jan Macku <jamacku@redhat.com>
2025-09-30 12:48:01 +0200

    fix: don't fail when issue parser returns array of arrays
    This is a workaround for issue in github issue parser: https://github.com/stefanbuck/github-issue-parser/issues/90

--- src/schema/input.ts
+++ src/schema/input.ts
@@ -2,7 +2,21 @@ import { z } from 'zod';
 
 export const issueFormSchema = z.record(
   z.string(),
-  z.string().or(z.array(z.string()))
+  z
+    .string()
+    .or(z.array(z.string()))
+    .or(z.array(z.array(z.unknown())))
+    .transform(value => {
+      // ?NOTE: This is just a workaround for issue in github issue parser: https://github.com/stefanbuck/github-issue-parser/issues/90
+      // Transform array of arrays into simple array
+      if (Array.isArray(value)) {
+        return value.map(item =>
+          Array.isArray(item) ? item.join(', ') : item
+        );
+      }
+
+      return value;
+    })
 );
 
 export type IssueFormType = z.infer<typeof issueFormSchema>;
